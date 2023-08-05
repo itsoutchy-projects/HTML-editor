4a6c04f3629d3661791a7cf48b5c2ad4cedb8d20
itsoutchy-projects <95544056+itsoutchy-projects@users.noreply.github.com>
2023-08-05 15:59:32 +0100

    Add site files

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", function(ev) {
+    var htmlArea = this.document.getElementById("htmlArea");
+    var frame = this.document.getElementById("result");
+    //console.log(htmlArea.innerText);
+    htmlArea.addEventListener("keypress", function(ev) {
+        //frame.innerHTML = "#document \n" + htmlArea.innerText;
+        frame.setAttribute("srcdoc", htmlArea.value);
+    });
+});
