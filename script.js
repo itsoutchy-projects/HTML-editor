e250e4bed81fe46b8e611130b3c3a6620bdf319d
itsoutchy-projects <95544056+itsoutchy-projects@users.noreply.github.com>
2023-08-24 13:23:51 +0100

    THIS should fix the annoying lag
    Last commit didn't do anything, so this should.
    
    I swear if this doesn't work, I'll be annoyed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function(ev) {
     var htmlArea = this.document.getElementById("htmlArea");
     var frame = this.document.getElementById("result");
     //console.log(htmlArea.innerText);
-    htmlArea.addEventListener("keyup", function(ev) {
+    htmlArea.addEventListener("input", function(ev) {
         //frame.innerHTML = "#document \n" + htmlArea.innerText;
         frame.setAttribute("srcdoc", htmlArea.value);
     });
