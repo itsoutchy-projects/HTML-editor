9b78d0b5a16fef413ae1c42a15b2509630246528
itsoutchy-projects <95544056+itsoutchy-projects@users.noreply.github.com>
2023-08-24 15:17:58 +0100

    Add support for indenting

--- script.js
+++ script.js
@@ -15,4 +15,21 @@ window.addEventListener("load", function(ev) {
             frame.setAttribute("srcdoc", htmlArea.value);
         }
     }
+
+    // INDENTING YAY
+    htmlArea.addEventListener("keydown", function(ev) {
+        if (ev.key == 'Tab') {
+            ev.preventDefault();
+            var start = this.selectionStart;
+            var end = this.selectionEnd;
+        
+            // set textarea value to: text before caret + tab + text after caret
+            this.value = this.value.substring(0, start) +
+              "\t" + this.value.substring(end);
+        
+            // put caret at right position again
+            this.selectionStart =
+              this.selectionEnd = start + 1;
+          }
+    });
 });
