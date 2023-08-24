0dd0aff4aba7e64212ba562233b78254efea4009
itsoutchy-projects <95544056+itsoutchy-projects@users.noreply.github.com>
2023-08-24 13:15:08 +0100

    Fix annoying lag
    Now the result iframe won't lag behind the textarea

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.addEventListener("load", function(ev) {
     var htmlArea = this.document.getElementById("htmlArea");
     var frame = this.document.getElementById("result");
     //console.log(htmlArea.innerText);
-    htmlArea.addEventListener("keypress", function(ev) {
+    htmlArea.addEventListener("keyup", function(ev) {
         //frame.innerHTML = "#document \n" + htmlArea.innerText;
         frame.setAttribute("srcdoc", htmlArea.value);
     });
