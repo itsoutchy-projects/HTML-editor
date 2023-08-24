4a1f8e389c3309a221e24f676a3f2f7bde8afa11
itsoutchy-projects <95544056+itsoutchy-projects@users.noreply.github.com>
2023-08-24 13:55:57 +0100

    Reinventing the updating
    Please tell me if this lags your browser, I had to do this because there is no other solution.
    
    I also do check if the code is different from the iframe's code, so it doesn't repeatedly reload the iframe.

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ window.addEventListener("load", function(ev) {
     var htmlArea = this.document.getElementById("htmlArea");
     var frame = this.document.getElementById("result");
     //console.log(htmlArea.innerText);
-    htmlArea.addEventListener("input", function(ev) {
-        //frame.innerHTML = "#document \n" + htmlArea.innerText;
-        frame.setAttribute("srcdoc", htmlArea.value);
-    });
+    // Reinventing it because I HATE the stupid delay the github pages site always gets, it's sooooooooooooooooooooooo annoying >:(
+    // htmlArea.addEventListener("input", function(ev) {
+    //     //frame.innerHTML = "#document \n" + htmlArea.innerText;
+    //     frame.setAttribute("srcdoc", htmlArea.value);
+    // });
+
+    this.setInterval(checkPleasePLEASE, 20); // We need to delay this a bit to keep it optimised
+    // We just repeatedly call this function now, PLEASE DO NOT LAG THE THING OUT IM TRYING SO HARD!!!!!!!!!!!!!!!!!!!!!!
+    function checkPleasePLEASE() {
+        if (htmlArea.value != frame.getAttribute("srcdoc")) { // Make sure that the user has actually typed some new code
+            frame.setAttribute("srcdoc", htmlArea.value);
+        }
+    }
 });
